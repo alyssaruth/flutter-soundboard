ce073ea666cd8aa81dead5d597e8d12d3d8d1065
Alex Burlton <alexburlton@hotmail.com>
2020-02-18 15:33:54 +0000

    Add groups for the quote tests

--- test/util/quote_test.dart
+++ test/util/quote_test.dart
@@ -7,88 +7,96 @@ import 'package:pocket_scat/util/quote.dart';
 import 'mocks.dart';
 
 void main() {
-  test('should always contain an empty search', () {
-    final quote = Quote("file_name", "bar", SRC_TOAST, "");
-    expect(quote.containsSearchTerm(""), true);
+  group('search', () {
+    test('should always contain an empty search', () {
+      final quote = Quote("file_name", "bar", SRC_TOAST, "");
+      expect(quote.containsSearchTerm(""), true);
+    });
+
+    test('should be searchable by name', () {
+      final quote = Quote("file_name", "Some Text", SRC_TOAST, "");
+      expect(quote.containsSearchTerm("Some Text"), true);
+      expect(quote.containsSearchTerm("sOmE tExT"), true);
+      expect(quote.containsSearchTerm("some"), true);
+      expect(quote.containsSearchTerm("text"), true);
+      expect(quote.containsSearchTerm("other text"), false);
+    });
+
+    test('should be searchable by search string', () {
+      final quote = Quote("file_name", "Some Text", SRC_TOAST, "the quote is actually this");
+      expect(quote.containsSearchTerm("the quote is actually this"), true);
+      expect(quote.containsSearchTerm("aCtUALLy"), true);
+      expect(quote.containsSearchTerm("tual"), true);
+      expect(quote.containsSearchTerm("not this"), false);
+    });
+
+    test('should be searchable by quote source', () {
+      final quote = Quote("file_name", "Some Text", SRC_TOAST, "");
+      expect(quote.containsSearchTerm("toast of london"), true);
+      expect(quote.containsSearchTerm("curb"), false);
+    });
+
+    test('should be searchable by quote category', () {
+      final quote = Quote("file_name", "Some Text", SRC_TOAST, "");
+      expect(quote.containsSearchTerm("sitcom"), true);
+      expect(quote.containsSearchTerm("kids tv"), false);
+    });
   });
 
-  test('should be searchable by name', () {
-    final quote = Quote("file_name", "Some Text", SRC_TOAST, "");
-    expect(quote.containsSearchTerm("Some Text"), true);
-    expect(quote.containsSearchTerm("sOmE tExT"), true);
-    expect(quote.containsSearchTerm("some"), true);
-    expect(quote.containsSearchTerm("text"), true);
-    expect(quote.containsSearchTerm("other text"), false);
+  group('getImage', () {
+    test('should return its own image path if one is specified', () {
+      final quote = Quote("file_name", "Some text", SRC_TOAST, "some search terms", "my_custom_image");
+      final img = quote.getImage();
+
+      expect(img.keyName, "assets/images/my_custom_image.png");
+    });
+
+    test('should fall back on source image path if not specified', () {
+      final quote = Quote("file_name", "Some text", SRC_TOAST, "some search terms");
+      final img = quote.getImage();
+
+      expect(img.keyName, "assets/images/toast.png");
+    });
   });
 
-  test('should be searchable by search string', () {
-    final quote = Quote("file_name", "Some Text", SRC_TOAST, "the quote is actually this");
-    expect(quote.containsSearchTerm("the quote is actually this"), true);
-    expect(quote.containsSearchTerm("aCtUALLy"), true);
-    expect(quote.containsSearchTerm("tual"), true);
-    expect(quote.containsSearchTerm("not this"), false);
+  group('playback', () {
+    test('should play the associated audio file', () async {
+      final quote = Quote("file_name", "Some text", SRC_TOAST, "");
+      audioCache = MockAudioCache();
+      await quote.play();
+
+      verify(audioCache.play("file_name.wav"));
+      verifyNoMoreInteractions(audioCache);
+    });
+
+    test('should stop the previous audio player if it exists before playing a new sound', () async {
+      final quoteOne = Quote("file_name", "Some text", SRC_TOAST, "");
+      final quoteTwo = Quote("other_file", "Other text", SRC_RAINBOW, "");
+
+      audioCache = MockAudioCache();
+      final audioPlayer = MockAudioPlayer();
+
+      when(audioCache.play(any)).thenAnswer((_) => Future.value(audioPlayer));
+
+      await quoteOne.play();
+      verify(audioCache.play("file_name.wav"));
+      verifyZeroInteractions(audioPlayer);
+
+      await quoteTwo.play();
+      verify(audioPlayer.stop());
+      verify(audioCache.play("other_file.wav"));
+    });
   });
 
-  test('should be searchable by quote source', () {
-    final quote = Quote("file_name", "Some Text", SRC_TOAST, "");
-    expect(quote.containsSearchTerm("toast of london"), true);
-    expect(quote.containsSearchTerm("curb"), false);
-  });
+  group('sharing', () {
+    test('should share the audio file', () async {
+      final quote = Quote("file_name", "Some text", SRC_TOAST, "");
+      final buildContext = MockBuildContext();
+      fileSharer = MockFileSharer();
 
-  test('should be searchable by quote category', () {
-    final quote = Quote("file_name", "Some Text", SRC_TOAST, "");
-    expect(quote.containsSearchTerm("sitcom"), true);
-    expect(quote.containsSearchTerm("kids tv"), false);
-  });
+      await quote.shareAudio(buildContext);
 
-  test('should return its own image path if one is specified', () {
-    final quote = Quote("file_name", "Some text", SRC_TOAST, "some search terms", "my_custom_image");
-    final img = quote.getImage();
-
-    expect(img.keyName, "assets/images/my_custom_image.png");
-  });
-
-  test('should fall back on source image path if not specified', () {
-    final quote = Quote("file_name", "Some text", SRC_TOAST, "some search terms");
-    final img = quote.getImage();
-
-    expect(img.keyName, "assets/images/toast.png");
-  });
-
-  test('should play the associated audio file', () async {
-    final quote = Quote("file_name", "Some text", SRC_TOAST, "");
-    audioCache = MockAudioCache();
-    await quote.play();
-
-    verify(audioCache.play("file_name.wav"));
-    verifyNoMoreInteractions(audioCache);
-  });
-
-  test('should stop the previous audio player if it exists before playing a new sound', () async {
-    final quoteOne = Quote("file_name", "Some text", SRC_TOAST, "");
-    final quoteTwo = Quote("other_file", "Other text", SRC_RAINBOW, "");
-
-    audioCache = MockAudioCache();
-    final audioPlayer = MockAudioPlayer();
-
-    when(audioCache.play(any)).thenAnswer((_) => Future.value(audioPlayer));
-
-    await quoteOne.play();
-    verify(audioCache.play("file_name.wav"));
-    verifyZeroInteractions(audioPlayer);
-
-    await quoteTwo.play();
-    verify(audioPlayer.stop());
-    verify(audioCache.play("other_file.wav"));
-  });
-
-  test('should share the audio file', () async {
-    final quote = Quote("file_name", "Some text", SRC_TOAST, "");
-    final buildContext = MockBuildContext();
-    fileSharer = MockFileSharer();
-
-    await quote.shareAudio(buildContext);
-
-    verify(fileSharer.shareFile("Some text", "file_name.wav", buildContext, "media/wav"));
+      verify(fileSharer.shareFile("Some text", "file_name.wav", buildContext, "media/wav"));
+    });
   });
 }
