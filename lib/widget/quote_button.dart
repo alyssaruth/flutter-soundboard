3b2d069435b2d9fe89b34f8712e94db389df7a93
Alex Burlton <alexburlton@hotmail.com>
2020-02-26 08:16:07 +0000

    Switch to using a stack so the text box can be a fixed height regardless of the button size

--- lib/widget/quote_button.dart
+++ lib/widget/quote_button.dart
@@ -13,36 +13,41 @@ class QuoteButton extends StatelessWidget {
         _quote.shareAudio(context);
       },
       child: CupertinoButton(
-        child: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-            image: new DecorationImage(image: _quote.getImage()),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Container(
-            alignment: Alignment.bottomCenter,
-            margin: EdgeInsets.fromLTRB(0, 140, 0, 0),
-            width: 200,
-            height: 40,
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(0, 0, 0, 0.75),
-                borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12))),
-            child: Container(
-                margin: EdgeInsets.fromLTRB(5, 0, 5, 8),
-                child: Text(
-                  _quote.name,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600),
-                )),
-          ),
-        ),
+        child: Stack(children: <Widget>[
+          Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                image: new DecorationImage(image: _quote.getImage()),
+                borderRadius: BorderRadius.circular(12),
+              )),
+          _buildTextOverlay(context)
+        ]),
         onPressed: () {
           _quote.play();
         },
+      ));
+
+  Widget _buildTextOverlay(BuildContext context) => Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        height: 40,
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 0, 0, 0.75),
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12))),
+        child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+            child: Text(
+              _quote.name,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600),
+            )),
       ));
 }
