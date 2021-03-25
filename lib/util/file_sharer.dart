5857acdab426bf088d26881b192fb11df086e081
Alex Burlton <alexburlton@hotmail.com>
2021-03-25 21:00:02 +0000

    replace share library with official one

--- lib/util/file_sharer.dart
+++ lib/util/file_sharer.dart
@@ -1,10 +1,17 @@
-import 'package:esys_flutter_share/esys_flutter_share.dart';
+import 'dart:io';
+
+import 'package:share/share.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:path_provider/path_provider.dart';
 
 class FileSharer {
   Future<void> shareFile(String name, String filename, BuildContext context, String mimeType) async {
     final bundle = DefaultAssetBundle.of(context);
     final bytes = await bundle.load('assets/$filename');
-    return Share.file(name, filename, bytes.buffer.asUint8List(), mimeType, text: name);
+    final tempDir = await getTemporaryDirectory();
+    final file = await File('${tempDir.path}/$filename').create();
+    await file.writeAsBytes(bytes.buffer.asUint8List());
+
+    return Share.shareFiles(['${tempDir.path}/$filename'], subject: name, mimeTypes: ['audio/wav']);
   }
 }
