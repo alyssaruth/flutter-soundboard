c156d515e7395537af0bbee9fd16ad8307488da5
Alex Burlton <alexburlton@hotmail.com>
2023-12-10 16:30:02 +0000

    new icon, start binning off quotes we don't want

--- lib/util/quote_category.dart
+++ lib/util/quote_category.dart
@@ -3,7 +3,6 @@ enum QuoteCategory { SKETCH, SITCOM, GAME, KIDS_TV, MISC }
 const Map<QuoteCategory, String> QuoteCategoryDesc = {
   QuoteCategory.SKETCH: 'Sketches',
   QuoteCategory.SITCOM: 'Sitcom',
-  QuoteCategory.GAME: 'Games',
   QuoteCategory.KIDS_TV: 'Kids TV',
   QuoteCategory.MISC: 'Misc',
 };
