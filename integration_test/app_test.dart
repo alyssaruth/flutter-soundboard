18db6850827e9b698aff60aac35f1b5d353bad7f
Alyssa <alyssa.burlton@gmail.com>
2023-12-24 10:53:57 +0000

    more dexter images, rm mitchell and webb

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,54 +2,48 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:pocket_scat/util/quote.dart';
-import 'package:pocket_scat/util/quote_source.dart';
+import 'package:pocket_scat/util/quotes_list.dart';
 import 'package:pocket_scat/widget/app.dart';
 
-const PIECE_OF_YOUR_BRAIN = Quote(
-    'fawlty_piece_of_your_brain', 'Piece of your brain', SRC_FAWLTY_TOWERS, 'Is this a piece of your brain Basil');
-const ERRONEOUS_DISH =
-    Quote('fawlty_erroneous_dish', 'Erroneous dish', SRC_FAWLTY_TOWERS, 'I have been given an erroneous dish');
-const VERY_NICE_BRIAN = Quote('bean_very_nice_brian', 'Very nice Brian', SRC_BEAN, 'very nice brian');
-const BAD_MISS = Quote('mitchell_bad_miss_1', 'Bad Miss 1', SRC_MITCHELL_AND_WEBB, 'Oh and thats a bad miss');
-
-const TEST_QUOTES = [PIECE_OF_YOUR_BRAIN, ERRONEOUS_DISH, VERY_NICE_BRIAN, BAD_MISS];
+final testQuotes = ALL_QUOTES.sublist(0, 5);
+final quoteA = testQuotes[0];
+final quoteB = testQuotes[1];
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('can filter by search and cancel search', (WidgetTester tester) async {
-    await tester.pumpWidget(const App(TEST_QUOTES));
+    await tester.pumpWidget(App(testQuotes));
 
-    expect(find.text('Erroneous dish'), findsOneWidget);
-    expect(find.text('Piece of your brain'), findsOneWidget);
+    expect(find.text(quoteA.name), findsOneWidget);
+    expect(find.text(quoteB.name), findsOneWidget);
 
     // Tap the search icon and enter text
     await tester.tap(find.byIcon(Icons.search));
     await tester.pump();
 
-    await tester.enterText(find.byType(TextField), 'piece');
+    await tester.enterText(find.byType(TextField), quoteB.name.substring(2));
     await tester.pump();
 
     // Verify that our search has been applied
-    expect(find.text('Erroneous dish'), findsNothing);
-    expect(find.text('Piece of your brain'), findsOneWidget);
+    expect(find.text(quoteA.name), findsNothing);
+    expect(find.text(quoteB.name), findsOneWidget);
 
     // Close the search
     await tester.tap(find.byIcon(Icons.close));
     await tester.pump();
 
     // Search should be cancelled
-    expect(find.text('Erroneous dish'), findsOneWidget);
-    expect(find.text('Piece of your brain'), findsOneWidget);
+    expect(find.text(quoteA.name), findsOneWidget);
+    expect(find.text(quoteB.name), findsOneWidget);
   });
 
   testWidgets('can play back quotes', (WidgetTester tester) async {
-    const app = App(TEST_QUOTES);
+    final app = App(testQuotes);
     await tester.pumpWidget(app);
     expect(app.getAudioPlayerState(), null);
 
-    await tester.tap(find.text('Piece of your brain'));
+    await tester.tap(find.text(quoteA.name));
     await tester.pumpAndSettle(const Duration(milliseconds: 500));
 
     expect(app.getAudioPlayerState(), PlayerState.PLAYING);
