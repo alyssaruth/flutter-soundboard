859a96a908dbaeb3c071bfedd5eeed19e35bf8ef
Alyssa <alyssa.burlton@gmail.com>
2023-12-28 16:02:27 +0000

    add cabin pressure

--- lib/util/quote_source.dart
+++ lib/util/quote_source.dart
@@ -16,11 +16,6 @@ class QuoteSource {
   }
 }
 
-const QuoteSource SRC_NO_MORE_JOCKEYS =
-    QuoteSource('No More Jockeys', QuoteCategory.SKETCH, 'nmj_horne', 'alex horne tim key mark watson watto');
-const QuoteSource SRC_NTNON = QuoteSource('Not the Nine O\'Clock News', QuoteCategory.SKETCH, 'ntnon');
-const QuoteSource SRC_MONTY_PYTHON = QuoteSource('Monty Python', QuoteCategory.SKETCH, 'monty_python');
-
 const QuoteSource SRC_HUSTLE = QuoteSource('Hustle', QuoteCategory.TV, 'hustle', 'hustle');
 const QuoteSource SRC_DEXTER = QuoteSource('Dexter', QuoteCategory.TV, 'doakes', 'dexter lundy doakes');
 const QuoteSource SRC_BEAN = QuoteSource('Mr. Bean', QuoteCategory.TV, 'bean', 'mr bean');
@@ -31,7 +26,12 @@ const QuoteSource SRC_DADS_ARMY = QuoteSource('Dad\'s Army', QuoteCategory.TV, '
 const QuoteSource SRC_B99 = QuoteSource('Brooklyn 99', QuoteCategory.TV, 'holt', 'raymond ray holt');
 
 const QuoteSource SRC_BARGAIN_HUNT = QuoteSource('Bargain Hunt', QuoteCategory.MISC, 'bargain_hunt', 'tim wonnacott');
+const QuoteSource SRC_CABIN_PRESSURE =
+    QuoteSource('Cabin Pressure', QuoteCategory.MISC, 'cabin_pressure', 'cabin pressure');
 const QuoteSource SRC_ICE_AGE = QuoteSource('Ice Age', QuoteCategory.MISC, 'ice_age', 'sid');
+const QuoteSource SRC_NO_MORE_JOCKEYS =
+    QuoteSource('No More Jockeys', QuoteCategory.MISC, 'nmj_horne', 'alex horne tim key mark watson watto');
+const QuoteSource SRC_NTNON = QuoteSource('Not the Nine O\'Clock News', QuoteCategory.MISC, 'ntnon');
 
 const QuoteSource SRC_NOAHS_ISLAND = QuoteSource('Noahs Island', QuoteCategory.KIDS_TV, 'sasha');
 const QuoteSource SRC_ART_ATTACK = QuoteSource('Art Attack', QuoteCategory.KIDS_TV, 'art_attack');
