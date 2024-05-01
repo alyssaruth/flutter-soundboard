b1dda917a65b8acb7e0cadb34434348549c62bf0
alyssa <alyssa.burlton@gmail.com>
2024-05-01 17:59:15 +0100

    move to new share package

--- lib/util/file_sharer.dart
+++ lib/util/file_sharer.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
-import 'package:share/share.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileSharer {
-  Future<void> shareFile(String name, String filename, BuildContext context, String mimeType) async {
+  Future<ShareResult> shareFile(String name, String filename, BuildContext context, String mimeType) async {
     final bundle = DefaultAssetBundle.of(context);
     final bytes = await bundle.load('assets/$filename');
     final tempDir = await getTemporaryDirectory();
     final file = await File('${tempDir.path}/$filename').create();
     await file.writeAsBytes(bytes.buffer.asUint8List());
 
-    return Share.shareFiles(['${tempDir.path}/$filename'], subject: name, mimeTypes: ['audio/wav']);
+    return Share.shareXFiles([XFile('${tempDir.path}/$filename', mimeType: mimeType, name: name)]);
   }
 }
