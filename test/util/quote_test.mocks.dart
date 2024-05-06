c05a797f75345cb4e339e482df3516834976fc34
alyssa <alyssa.burlton@gmail.com>
2024-05-06 10:58:26 +0100

    get all dependencies onto latest versions, document mockito thing

--- test/util/quote_test.mocks.dart
+++ test/util/quote_test.mocks.dart
@@ -96,6 +96,22 @@ class MockAudioPlayer extends _i1.Mock implements _i2.AudioPlayer {
       ) as String);
 
   @override
+  _i2.PlayerState get desiredState => (super.noSuchMethod(
+        Invocation.getter(#desiredState),
+        returnValue: _i2.PlayerState.stopped,
+        returnValueForMissingStub: _i2.PlayerState.stopped,
+      ) as _i2.PlayerState);
+
+  @override
+  set desiredState(_i2.PlayerState? _desiredState) => super.noSuchMethod(
+        Invocation.setter(
+          #desiredState,
+          _desiredState,
+        ),
+        returnValueForMissingStub: null,
+      );
+
+  @override
   _i3.Completer<void> get creatingCompleter => (super.noSuchMethod(
         Invocation.getter(#creatingCompleter),
         returnValue: _FakeCompleter_1<void>(
@@ -207,6 +223,15 @@ class MockAudioPlayer extends _i1.Mock implements _i2.AudioPlayer {
         returnValue: _i3.Stream<String>.empty(),
         returnValueForMissingStub: _i3.Stream<String>.empty(),
       ) as _i3.Stream<String>);
+
+  @override
+  set positionUpdater(_i2.PositionUpdater? positionUpdater) => super.noSuchMethod(
+        Invocation.setter(
+          #positionUpdater,
+          positionUpdater,
+        ),
+        returnValueForMissingStub: null,
+      );
 
   @override
   _i3.Future<void> play(
@@ -354,40 +379,60 @@ class MockAudioPlayer extends _i1.Mock implements _i2.AudioPlayer {
       ) as _i3.Future<void>);
 
   @override
-  _i3.Future<void> setSourceUrl(String? url) => (super.noSuchMethod(
+  _i3.Future<void> setSourceUrl(
+    String? url, {
+    String? mimeType,
+  }) =>
+      (super.noSuchMethod(
         Invocation.method(
           #setSourceUrl,
           [url],
+          {#mimeType: mimeType},
         ),
         returnValue: _i3.Future<void>.value(),
         returnValueForMissingStub: _i3.Future<void>.value(),
       ) as _i3.Future<void>);
 
   @override
-  _i3.Future<void> setSourceDeviceFile(String? path) => (super.noSuchMethod(
+  _i3.Future<void> setSourceDeviceFile(
+    String? path, {
+    String? mimeType,
+  }) =>
+      (super.noSuchMethod(
         Invocation.method(
           #setSourceDeviceFile,
           [path],
+          {#mimeType: mimeType},
         ),
         returnValue: _i3.Future<void>.value(),
         returnValueForMissingStub: _i3.Future<void>.value(),
       ) as _i3.Future<void>);
 
   @override
-  _i3.Future<void> setSourceAsset(String? path) => (super.noSuchMethod(
+  _i3.Future<void> setSourceAsset(
+    String? path, {
+    String? mimeType,
+  }) =>
+      (super.noSuchMethod(
         Invocation.method(
           #setSourceAsset,
           [path],
+          {#mimeType: mimeType},
         ),
         returnValue: _i3.Future<void>.value(),
         returnValueForMissingStub: _i3.Future<void>.value(),
       ) as _i3.Future<void>);
 
   @override
-  _i3.Future<void> setSourceBytes(_i6.Uint8List? bytes) => (super.noSuchMethod(
+  _i3.Future<void> setSourceBytes(
+    _i6.Uint8List? bytes, {
+    String? mimeType,
+  }) =>
+      (super.noSuchMethod(
         Invocation.method(
           #setSourceBytes,
           [bytes],
+          {#mimeType: mimeType},
         ),
         returnValue: _i3.Future<void>.value(),
         returnValueForMissingStub: _i3.Future<void>.value(),
